8a0d41b6ab0735f87075ef38e0aa8b6004305f99
juancatap7 <102172377+juancatap7@users.noreply.github.com>
2022-05-12 12:43:02 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,28 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contáctanos</h4>
+                <a href="https://www.facebook.com/Sheindel-Garcia-Boutique-Oficial-101648585506476" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="https://goo.gl/maps/meVTk4CoMx3yeWhHA" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; ¡Encuéntranos!</i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Horario</h4>
+                <p style="color: #FFF">Lunes a Sábado 9:30am  a 7:00pm</p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >67250, Lib. Julio Cisneros 300, Centro de Benito Juárez, Juárez, N.L.</h4>
+                <p style="color: #FFF">México</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  81 23 79 33 51 / 81 32 44 95 30</p>
+            </div>
+        </div>
+    </div>
+    <br>
+    <h5 class="text-center tittles-pages-logo text-footer">SHEINDEL GARCÍA Boutique &copy; 2022</h5>
+</footer>
